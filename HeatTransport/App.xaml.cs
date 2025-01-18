f4150c56a62ca58b530db5439a824de405a3ecd3
Xenoneqq <realfatboimlg@gmail.com>
2025-01-18 18:29:00 +0100

    Major refactor of the project

---
+++ HeatTransport/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace HeatTransport
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+
+            int elements = 6;
+            Calculator calculator = new Calculator();
+            var result = calculator.Calculate(elements);
+            ResultPrinter resultPrint = new ResultPrinter();
+
+            List<double> x = result.Item1;
+            List<double> y = result.Item2;
+            resultPrint.SaveToFile(x,y);
+
+            window.Draw(x, y);
+            window.Show();
+        }
+    }
+}
---
+++ HeatTransport/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace HeatTransport
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw(List<double> x, List<double> y)
+        {
+            if (x.Count != y.Count)
+            {
+                throw new System.ArgumentException("X and Y lists must have the same length.");
+            }
+
+            // Setting data series
+            var series = new LineSeries
+            {
+                Values = new ChartValues<double>(y),
+                Title = "Function",
+                PointGeometry = null
+            };
+
+            // Setting X axis
+            chart.AxisX.Add(new Axis
+            {
+                Title = "X",
+                Labels = x.ConvertAll(v => v.ToString("0.00"))
+            });
+
+            // Setting Y axis
+            chart.AxisY.Add(new Axis
+            {
+                Title = "Y"
+            });
+
+            // Painting the series to the graph
+            chart.Series = new SeriesCollection { series };
+        }
+    }
+}
